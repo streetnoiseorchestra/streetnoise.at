81aa2743cd817814d368a3b249af0718cb05c84c
Casey Link <me@caseylink.com>
2019-05-04 21:28:24 +0200

    Correctly set the database url

--- streetnoise/settings/production.py
+++ streetnoise/settings/production.py
@@ -37,4 +37,4 @@ try:
 except ImportError:
     pass
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
