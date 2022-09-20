937996a4961593b99622589c292ff3b25e5776f5
Casey Link <unnamedrambler@gmail.com>
2022-09-20 16:52:42 +0200

    Remove taggit migration for newsletter

--- newsletter/migrations/0001_initial.py
+++ newsletter/migrations/0001_initial.py
@@ -11,19 +11,66 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('birdsong', '0007_alter_contacttag_tag'),
-        ('wagtailimages', '0024_index_image_file_hash'),
+        ("wagtailimages", "0024_index_image_file_hash"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Newsletter',
+            name="Newsletter",
             fields=[
-                ('campaign_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='birdsong.campaign')),
-                ('headline', models.CharField(help_text='The headline to use for the newsletter.', max_length=255)),
-                ('body', wagtail.fields.StreamField([('rich_text', wagtail.blocks.RichTextBlock(features=['h3', 'h4', 'bold', 'italic', 'link', 'ul', 'ol', 'document-link'], template='birdsong/mail/blocks/richtext.html'))], use_json_field=None)),
-                ('header_background', models.ForeignKey(help_text='The image to use for the header backgound.', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='wagtailimages.image')),
+                (
+                    "campaign_ptr",
+                    models.OneToOneField(
+                        auto_created=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        parent_link=True,
+                        primary_key=True,
+                        serialize=False,
+                        to="birdsong.campaign",
+                    ),
+                ),
+                (
+                    "headline",
+                    models.CharField(
+                        help_text="The headline to use for the newsletter.",
+                        max_length=255,
+                    ),
+                ),
+                (
+                    "body",
+                    wagtail.fields.StreamField(
+                        [
+                            (
+                                "rich_text",
+                                wagtail.blocks.RichTextBlock(
+                                    features=[
+                                        "h3",
+                                        "h4",
+                                        "bold",
+                                        "italic",
+                                        "link",
+                                        "ul",
+                                        "ol",
+                                        "document-link",
+                                    ],
+                                    template="birdsong/mail/blocks/richtext.html",
+                                ),
+                            )
+                        ],
+                        use_json_field=None,
+                    ),
+                ),
+                (
+                    "header_background",
+                    models.ForeignKey(
+                        help_text="The image to use for the header backgound.",
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        related_name="+",
+                        to="wagtailimages.image",
+                    ),
+                ),
             ],
-            bases=('birdsong.campaign',),
+            bases=("birdsong.campaign",),
         ),
     ]
